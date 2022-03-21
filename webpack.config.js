6cda0ee35908b6cc515eb4848d65a06a54f16e49
tatsuya.kitabori <45zombie45@gmail.com>
2022-03-21 20:51:36 +0900

    reactの雛形実装

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
