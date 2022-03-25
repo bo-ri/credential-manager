4a7c71f36708506c199478aa7c9c988e887fa7eb
tatsuya.kitabori <45zombie45@gmail.com>
2022-03-25 23:08:44 +0900

    table最低限の表示

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,46 @@
 import React from "react";
 import {
   ChakraProvider,
-  SimpleGrid
+  SimpleGrid,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Tbody,
+  Checkbox,
+  Switch
 } from '@chakra-ui/react'
 import credentials from "../credentials.json";
+import "./styles.css"
 
-const CredentialsTable = () => {
+const CredentialsTable = ({ credentials }) => {
   return (
     <div>
+      <Table variant={"simple"}>
+        <Thead>
+          <Tr>
+            <Th></Th>
+            <Th>credential name</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {Object.keys(credentials).map((credential) => {
+            return (
+              <Tr key={credential}>
+              <Td><Checkbox defaultChecked></Checkbox></Td>
+              <Td>{credential}</Td>
+              <Td>
+                <SimpleGrid row={2}>
+                  <Switch colorSheme={"gray"} className={"Centering"}></Switch>
+                  <span className={"Centering"}>dev/prod</span>
+                </SimpleGrid>
+              </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
     </div>
   );
 }
@@ -24,7 +57,7 @@ export const App = () => {
   return (
     <ChakraProvider>
       <SimpleGrid columns={2}>
-        <CredentialsTable />
+        <CredentialsTable credentials={credentials} />
         <JsonField />
       </SimpleGrid>
     </ChakraProvider>
